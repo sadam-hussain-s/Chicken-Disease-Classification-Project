ed9e9c27797e166efab03a4652c89ce12d97f364
sadam-hussain-s <sadam89391@gmail.com>
2024-04-20 11:06:53 +0530

    training added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -24,8 +24,9 @@ class PrepareCallback:
 
     @property
     def _create_ckpt_callbacks(self):
+        checkpoint_model_filepath_str = str(self.config.checkpoint_model_filepath)
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.config.checkpoint_model_filepath,
+            filepath=checkpoint_model_filepath_str,
             save_best_only=True
         )
 
